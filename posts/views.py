0a5d50efeef30e580c2f15ceb7bd7b387293d106
KerlyLaeda <olgaperchenko1999@gmail.com>
2024-12-20 15:38:07 +0400

    build(posts): implement post creation, modification and filtering
    - add views to display, create, update, and delete posts and viewing
      post's details
    - handle permissions for updating and deleting posts
    - filter posts by author
    - add pagination

--- posts/views.py
+++ posts/views.py
@@ -1,26 +1,76 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView
+)
 from .models import Post
-
-
-dummy = [
-    {
-        "author": "ferogerg",
-        "title": "jijirjirgjri",
-        "body": "dhfgifjorfjof0",
-        "date_posted": "11/4/2068"
-    },
-    {
-        "author": "wgthtrhtg",
-        "title": "gtrhrhtyjy",
-        "body": "ghtjtyuj",
-        "date_posted": "15/8/2069"
-    }
-]
 
 
 def index(request):
     context = {
-        # "posts": Post.objects.all()
-        "posts": dummy
+        "posts": Post.objects.all()
     }
     return render(request, "posts/index.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/index.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+    paginate_by = 10
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "posts/user_posts.html"
+    context_object_name = "posts"
+    paginate_by = 10
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-date_posted")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "body"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "body"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
